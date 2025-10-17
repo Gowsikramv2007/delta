f82ca651cd7bf6a2b08da3ed0cc98853a68d57d3
Gowsikramv2007 <gowsikramv583224148016@nprcolleges.org>
2025-10-17 11:48:51 +0530

    last one  delta.py

--- delta.py
+++ delta.py
@@ -1,11 +1,13 @@
 
-age=input("welcome Enter your age!")
-try:
-        age=int(age)
-if age >= 18:
-        print("You are eligible to vote in Tamilnadu election")
-else:
-        print("Sorry, you are not old enough to vote in Tamilnadu.")
-except ValueError:
-    print("Please enter a valid number for your age.")
+age = input("Welcome! Please enter your age: ")
 
+try:
+    age = int(age)
+
+ 
+    if age >= 18:
+        print("✅ You are eligible to vote in Tamil nadu election. Make your voice count!")
+    else:
+        print("⛔ Sorry, you are not old enough to vote in Tamil nadu election yet.")
+except ValueError:
+    print("⚠️ Please enter a valid number for your age.")
